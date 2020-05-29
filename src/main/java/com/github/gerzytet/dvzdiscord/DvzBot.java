6f829cadbd7d8ae0b577111f15d07193a525dc19
Craig White <gerzytet@gmail.com>
2020-05-29 11:59:03 -0400

    All the stuff.

--- src/main/java/com/github/gerzytet/dvzdiscord/DvzBot.java
+++ src/main/java/com/github/gerzytet/dvzdiscord/DvzBot.java
@@ -1,6 +1,9 @@
 package com.github.gerzytet.dvzdiscord;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
 import javax.security.auth.login.LoginException;
 
@@ -69,15 +72,127 @@ public class DvzBot {
 		bot.shutdown();
 	}
 	
+	//private static char[] clocks;
+	
+	/*
+	static {
+		clocks = new char[12];
+		//one o'clock
+		char clock = (char) 128336;
+		for (int i = 0; i < 12; i++) {
+			clocks[i] = clock;
+			clock++;
+		}
+	}*/
+	
+	private static String clockFromDouble(double d) {
+		return ":clock" + Math.max(Math.round(d * 12), 1) + ":";
+	}
+	
+	private static String TROPHY = ":trophy:";
+	private static String CHECK = ":white_check_mark:";
+	private static String SWORD = ":crossed_swords:";
+	private static String MOON = ":crescent_moon:";
+	private static String STOP = ":octagonal_sign:";
+	
 	private void send(String message) {
 		channel.sendMessage(message).queue();
 	}
 	
 	/**
+	 * Announce game start
 	 * 
 	 * @param players players online
 	 */
 	public void start(int players) {
-		send("DvZ is starting with " + players + " players");
+		send(CHECK + " DvZ started with " + players + " players");
+	}
+	
+	private String formatPlayers(int dwarves, int monsters, int assassins) {
+		return dwarves + " dwarves, " + monsters + " monsters, and " + assassins + " assassins";
+	}
+	
+	private String formatMinutesSeconds(int minutes, int seconds) {
+		return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+	}
+	
+	/**
+	 * Sends a status report during the daytime phase
+	 * 
+	 * @param dwarves number of dwarves online
+	 * @param secondsUntilNight seconds until monsters are released
+	 * @param totalDaytimeSeconds the total seconds that daytime lasts
+	 */
+	public void daytimeStatus(int dwarves, int secondsUntilNight, int totalDaytimeSeconds) {
+		int minutes = secondsUntilNight / 60;
+		int seconds = secondsUntilNight % 60;
+		send(clockFromDouble(1 - ((double) secondsUntilNight / totalDaytimeSeconds)) + " " + 
+		     dwarves + " dwarves online.  " + formatMinutesSeconds(minutes, seconds) + " until the monsters are released.");
+	}
+	
+	/**
+	 * Announce monster release
+	 */
+	public void nightfall() {
+		send(MOON + " The monsters were released!");
+	}
+	
+	/**
+	 * Sends a status report during the nighttime phase
+	 * 
+	 * @param dwarves number of dwarves
+	 * @param monsters number of monsters
+	 * @param assassins number of assassins
+	 * @param secondsUntilDwarvesWin
+	 * @param totalNighttimeSeconds total number of seconds 
+	 */
+	public void nighttimeStatus(int dwarves, int monsters, int assassins, int secondsUntilDwarvesWin, int totalNighttimeSeconds) {
+		int minutes = secondsUntilDwarvesWin / 60;
+		int seconds = secondsUntilDwarvesWin % 60;
+		send(clockFromDouble(1 - ((double) secondsUntilDwarvesWin / totalNighttimeSeconds)) + " " +
+	         formatPlayers(dwarves, monsters, assassins) + " online.  " + formatMinutesSeconds(minutes, seconds) + " until dwarves win.");
+	}
+	
+	/**
+	 * Announce shrine change.
+	 * 
+	 * @param shrine The new shrine.  either 2 or 3.
+	 */
+	public void changeShrine(int shrine) {
+		String x = "";
+		if (shrine == 2) {
+			x = "first";
+		} else if (shrine == 3) {
+			x = "second";
+		}
+		send(SWORD + " The monsters conquered the " + x + " shrine");
+	}
+	
+	/**
+	 * Announce game end
+	 * 
+	 * @param reason
+	 */
+	public void endGame(EndReason reason) {
+		String msg = "";
+		switch (reason) {
+		case MONSTERS_WON:
+			msg = TROPHY + "the monsters won!";
+			break;
+		case DWARVES_WON:
+			msg = TROPHY + " the dwarves won!";
+			break;
+		case SERVER_RESET:
+			msg = STOP + " the server stopped.";
+			break;
+		default:
+			break;
+			
+		}
+		send(msg);
+	}
+	
+	public static enum EndReason {
+		MONSTERS_WON, DWARVES_WON, SERVER_RESET
 	}
 }
